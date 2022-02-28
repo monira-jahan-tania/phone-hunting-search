7dd4df4bff250179aa137ec337dcbc71c274d832
monira-jahan-tania <tania598330@gmail.com>
2022-03-01 00:35:22 +0600

    details done

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const showResult = phones => {
     console.log(phones)
     const main = document.getElementById('main')
     phones.slice(0, 20).forEach(phone => {
-        console.log(phone)
         const div = document.createElement('div')
         div.classList.add('col-lg-4')
         div.classList.add('gy-5')
@@ -22,10 +21,37 @@ const showResult = phones => {
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">${phone.brand}</p>
-                <a href="#" class="btn btn-primary">Show Details</a>
+                
+                <a href="#" onclick="detailsLoad('${phone.slug}')" class="btn btn-primary">Show Details</a>
             </div>
         </div>
         `
         main.appendChild(div)
     })
+}
+
+const detailsLoad = (phoneId) => {
+    console.log(phoneId)
+    fetch(`https://openapi.programming-hero.com/api/phone/${phoneId}`)
+        .then(res => res.json())
+        .then(data => showDetails(data.data))
+}
+const showDetails = phoneDetails => {
+    console.log(phoneDetails)
+
+    const details = document.getElementById('details')
+
+    const div = document.createElement('div')
+    div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img src="${phoneDetails.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phoneDetails.brand}</h5>
+                <p class="card-text">${phoneDetails.releaseDate}</p>
+                <p class="card-text">${phoneDetails.mainFeatures.storage}</p>
+            </div>
+        </div>
+        `
+    details.appendChild(div)
+
 }
