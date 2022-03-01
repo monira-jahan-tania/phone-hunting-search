49046fac2d006eafd5fe9d65b8bb2c727c86b195
monira-jahan-tania <tania598330@gmail.com>
2022-03-01 22:28:35 +0600

    excluding optional

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const main = document.getElementById('main')
 const details = document.getElementById('details')
 
+
+// loading spinner
+const loadingSpinner = (para) => {
+    const spinner = document.getElementById('spinner')
+    spinner.style.display = para
+}
 // search data loading
 const searchPhone = () => {
     const searchItem = document.getElementById('search-item')
     const searchText = searchItem.value
     document.getElementById('search-item').value = ''
 
+    loadingSpinner('block')
+    main.innerHTML = ''
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
         .then(res => res.json())
         .then(data => showResult(data.data))
@@ -25,7 +33,7 @@ const showResult = phones => {
         div.classList.add('col')
         div.classList.add('gy-5')
         div.innerHTML = `
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 15rem;">
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
@@ -37,6 +45,8 @@ const showResult = phones => {
         `
         main.appendChild(div)
     })
+
+    loadingSpinner('none')
 }
 
 
@@ -64,23 +74,20 @@ const showDetails = phoneDetails => {
             <img src="${phoneDetails.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h2 class="card-title">${phoneDetails.brand}</h2>
-                <p class="card-text">Release Date: ${phoneDetails.releaseDate ? phoneDetails.releaseDate : 'Not Found'}</p>
+                <p class="card-text"><span>Release Date:</span> ${phoneDetails.releaseDate ? phoneDetails.releaseDate : 'Not Found'}</p>
 
                 <div>
                     <h4>Main Features</h4>
-                    <p class="card-text">chipSet:  ${phoneDetails.mainFeatures.chipSet}</p>
-                    <p class="card-text">displaySize:  ${phoneDetails.mainFeatures.displaySize}</p>
-                    <p class="card-text">memory: ${phoneDetails.mainFeatures.memory}</p>
-                    <p class="card-text">storage:  ${phoneDetails.mainFeatures.storage}</p>
-                
-                    <p class="card-text">sensors: ${phoneDetails.mainFeatures.sensors}</p>
-                    <h4>Others: N/A</h4>
+                    <p class="card-text"><span>chip-set:</span>  ${phoneDetails.mainFeatures.chipSet}</p>
+                    <p class="card-text"><span>display-size:</span>  ${phoneDetails.mainFeatures.displaySize}</p>
+                    <p class="card-text"><span>memory:</span>  ${phoneDetails.mainFeatures.memory}</p>
+                    <p class="card-text"><span>storage:</span>  ${phoneDetails.mainFeatures.storage}</p>
+            
+                    <p class="card-text"><span>sensors:</span> ${phoneDetails.mainFeatures.sensors}</p>
+                    <p><span>Others:</span> N/A</p>
 
 
-                </div>
-
-
-               
+                </div> 
             </div>
         </div>
         `
@@ -94,33 +101,31 @@ const showDetails = phoneDetails => {
         div.innerHTML = `
         <div class="card" style="width: 25rem;">
             <img src="${phoneDetails.image}" class="card-img-top" alt="...">
-            <div class="card-body">
+            <div class="card-body card">
                 <h2 class="card-title">${phoneDetails.brand}</h2>
-                <p class="card-text">Release Date: ${phoneDetails.releaseDate ? phoneDetails.releaseDate : 'Not Found'}</p>
+                <p class="card-text"><span>Release Date:</span> ${phoneDetails.releaseDate ? phoneDetails.releaseDate : 'Not Found'}</p>
 
                 <div>
                     <h4>Main Features</h4>
-                    <p class="card-text">chipSet:  ${phoneDetails.mainFeatures.chipSet}</p>
-                    <p class="card-text">displaySize:  ${phoneDetails.mainFeatures.displaySize}</p>
-                    <p class="card-text">memory:  ${phoneDetails.mainFeatures.memory}</p>
-                    <p class="card-text">storage:  ${phoneDetails.mainFeatures.storage}</p>
+                    <p class="card-text"><span>chip-set:</span>  ${phoneDetails.mainFeatures.chipSet}</p>
+                    <p class="card-text"><span>display-size:</span>  ${phoneDetails.mainFeatures.displaySize}</p>
+                    <p class="card-text"><span>memory:</span>  ${phoneDetails.mainFeatures.memory}</p>
+                    <p class="card-text"><span>storage:</span>  ${phoneDetails.mainFeatures.storage}</p>
                 
-                    <p class="card-text">sensors: ${phoneDetails.mainFeatures.sensors}</p>
+                    <p class="card-text"><span>sensors:</span> ${phoneDetails.mainFeatures.sensors}</p>
 
 
                 </div>
 
                 <div>
                     <h4>Others</h4>
-                    <p class="card-text">Bluetooth: ${phoneDetails.others.Bluetooth ? phoneDetails.others.Bluetooth : ''}</p>
-                    <p class="card-text">GPS: ${phoneDetails.others.GPS ? phoneDetails.others.GPS : ''}</p>
-                    <p class="card-text">NFC: ${phoneDetails.others.NFC ? phoneDetails.others.NFC : ''}</p>
-                    <p class="card-text">Radio: ${phoneDetails.others.Radio ? phoneDetails.others.Radio : ''}</p>
-                    <p class="card-text">USB: ${phoneDetails.others.USB ? phoneDetails.others.USB : ''}</p>
-                    <p class="card-text">WLAN: ${phoneDetails.others.WLAN ? phoneDetails.others.WLAN : ''}</p>
-                </div>
-
-               
+                    <p class="card-text"><span>Bluetooth:</span> ${phoneDetails.others.Bluetooth ? phoneDetails.others.Bluetooth : ''}</p>
+                    <p class="card-text"><span>GPS:</span> ${phoneDetails.others.GPS ? phoneDetails.others.GPS : ''}</p>
+                    <p class="card-text"><span>NFC:</span> ${phoneDetails.others.NFC ? phoneDetails.others.NFC : ''}</p>
+                    <p class="card-text"><span>Radio:</span> ${phoneDetails.others.Radio ? phoneDetails.others.Radio : ''}</p>
+                    <p class="card-text"><span>USB:</span> ${phoneDetails.others.USB ? phoneDetails.others.USB : ''}</p>
+                    <p class="card-text"><span>WLAN:</span> ${phoneDetails.others.WLAN ? phoneDetails.others.WLAN : ''}</p>
+                </div>   
             </div>
         </div>
         `
@@ -129,27 +134,3 @@ const showDetails = phoneDetails => {
     }
 }
 
-// const showOthers = others => {
-//     const div = document.getElementById('others-show')
-//     for (const other in others) {
-//         const p = document.createElement('p')
-//         p.innerText = other
-//         div.appendChild(p)
-//     }
-// }
-
-// const showSensor = sensorInfos => {
-//     const divOfSensor = document.getElementById('sensor-show')
-//     for (const info of sensorInfos) {
-
-//         const p = document.createElement('p')
-//         p.innerText = info
-//         divOfSensor.appendChild(p)
-//     }
-// }
-
-/* <p class="card-text">sensor:  showSensor(${phoneDetails.mainFeatures.sensors})</p> */
-
-// <button onclick="showOthers('${phoneDetails.others ? phoneDetails.others : ""}')">others</button>
-// <div id="others-show"></div>
-// <p class="card-text">others: ${phoneDetails.others ? phoneDetails.others : ''}</p>
